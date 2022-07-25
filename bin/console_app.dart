3f1fc3a1fa49fa4f6bd8c4534490ef3f68c82516
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2022-07-25 16:19:47 +0500

    new reset

--- bin/console_app.dart
+++ bin/console_app.dart
@@ -1,5 +1,5 @@
 import 'package:console_app/console_app.dart' as console_app;
 
 void main(List<String> arguments) {
-  print('Hello world salom dunyo: ${console_app.calculate(9, 8)}!');
+  print('Hello dunyo: ${console_app.calculate(9, 8)}!');
 }

32c75067c7b23cf61569a09f26afbd983a60e25b
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2022-07-25 16:11:39 +0500

    ok

--- bin/console_app.dart
+++ bin/console_app.dart
@@ -1,3 +1,5 @@
+import 'package:console_app/console_app.dart' as console_app;
+
 void main(List<String> arguments) {
-  print('Hello world');
+  print('Hello world: ${console_app.calculate()}!');
 }
